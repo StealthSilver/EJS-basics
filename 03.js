230c65489c507509d945b68f6cadf049637ab268
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-11-28 13:12:48 +0530

    final commit

--- 03.js
+++ 03.js
@@ -3,6 +3,12 @@ const app = express();
 const path = require("path")
 const port = 3000;
 
+
+app.use(express.static(path.join(__dirname, "public/css")))
+app.use(express.static(path.join(__dirname, "public/js")))
+// app.use(express.static("public"));
+// to get all the static files which can be css or json, they must be in the public directory
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -13,8 +19,13 @@ app.get("/ig/:username", (req, res) => {
     let { username } = req.params
     const instaData = require("./data.json");
     const data = instaData[username];
+    if (data) {
+        res.render("insta.ejs", { data });
+    }
+    else {
+        res.render("error.ejs")
+    }
 
-    res.render("insta.ejs", { data });
 })
 
 
